fe1ba7fb286252cf00ecadd66f728d4b7d5937ad
Mohammed Alnahdi <mohammed-alnahdi@protonmail.com>
2022-01-10 02:04:43 +0300

    the initalization

---
+++ config.def.h
@@ -0,0 +1,27 @@
+static char first_program_name[] = "name_app";
+static char call_first_program[] = "app";
+
+static char second_program_name[] = "name_app";
+static char call_second_program[] = "app";
+
+static char third_program_name[] = "name_app";
+static char call_third_program[] = "app";
+
+static char fourth_program_name[] = "name_app";
+static char call_fourth_program[] = "app";
+
+static char fifth_program_name[] = "name_app";
+static char call_fifth_program[] = "app";
+
+static char sixth_program_name[] = "name_app";
+static char call_sixth_program[] = "app";
+
+static char seventh_program_name[] = "name_app";
+static char call_seventh_program[] = "app";
+
+
+static char eight_program_name[] = "name_app";
+static char call_eight_program[] = "app";
+
+static char nine_program_name[] = "name_app";
+static char call_nine_program[] = "app";
